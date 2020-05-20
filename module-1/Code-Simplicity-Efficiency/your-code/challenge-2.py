2e9158c3227e1eb1697e9118eed3384162783882
Ferenc Lippai <ferenc.lippai@gmail.com>
2020-05-20 08:44:27 +0200

    'github_api_sucks'

--- module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
+++ module-1/Code-Simplicity-Efficiency/your-code/challenge-2.py
@@ -7,32 +7,39 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+# %load challenge-2.py
+"""
+The code below generates a given number of random strings that consists of numbers and 
+lower case English letters. You can also define the range of the variable lengths of
+the strings being generated.
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+The code is functional but has a lot of room for improvement. Use what you have learned
+about simple and efficient code, refactor the code.
+"""
+
+import string
+import random
+import sys
+
+def random_string(length_low, length_high, batches):
+    final_strings = []
+    charset = string.ascii_letters + string.digits
+
+    for i in range(batches):
+        buffer = '' 
+        if length_low==length_high:
+            length=length_low
+        elif length_low>length_high:
+            sys.exit('input low cannot be higher than input high')
         else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+            length = random.choice(range(length_low, length_high))
+        for i in range(length):
+            char = random.choice(charset)
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+            buffer+=char
+        final_strings.append(buffer)
+    print(final_strings)
+random_string(1,3,2)
+
+
